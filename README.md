56f29a9de3448ad0e8cf0cb606d7b775d661160b
Ali_A109 <ali_a109@yahoo.com>
2023-07-30 12:23:21 -0700

    my project commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Word Game
+
+This is a simple word game similar to Wordle
+
+You have to guess a 4 word letter and you have 5 attempts
+
+If the letter is green it's in the correct place
+
+If the letter is orange it's in the wrong place
+
+If the letter is gray it's not in the secret word
---
+++ public/app.js
@@ -0,0 +1,74 @@
+let words = [
+  "Bank",
+  "City",
+  "Coat",
+  "Face",
+  "Fish",
+  "Gold",
+  "Iron",
+  "Nose",
+  "Hair",
+  "Cash",
+];
+const randomIndex = Math.floor(Math.random() * 9);
+let word = words[randomIndex].toUpperCase();
+console.log(word);
+let row = 1;
+let column = 1;
+
+function onKeyClick(e) {
+  console.log(e.target.innerHTML);
+
+  const selector = `.tile-${row}-${column}`;
+
+  const tile = document.querySelector(selector);
+  if (!tile) {
+    return;
+  }
+
+  tile.innerHTML = e.target.innerHTML;
+
+  column = column + 1;
+}
+
+function onEnter(e) {
+
+  let answer = "";
+  for (let i = 0; i < 4; i++) {
+    let selector = `.tile-${row}-${i + 1}`;
+    const tile = document.querySelector(selector);
+    answer = answer + tile.innerHTML;
+    setCssClass(tile, word.charAt(i));
+  }
+
+  if (answer === word) {
+    document.querySelector(".win-dialog").showModal();
+    return;
+  }
+  if(row === 5){
+    document.querySelector(".lose-dialog").showModal();
+    return;
+  }
+
+  row = row + 1;
+  column = 1;
+}
+
+function setCssClass(tile, letter) {
+  if (tile.innerHTML === letter) tile.classList.add("tile-green");
+  else if (word.indexOf(tile.innerHTML) > -1) tile.classList.add("tile-yellow");
+  else tile.classList.add("tile-gray");
+}
+
+function onBackSpace(e) {
+  const selector = `.tile-${row}-${column-1}`;
+
+  const tile = document.querySelector(selector);
+  if (!tile) {
+    return;
+  }
+
+  tile.innerHTML = "";
+
+  column = column - 1;
+}
